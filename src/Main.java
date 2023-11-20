4d93acd1b4ab9b8f5a763f59e0e4f1cbbb166083
Dwaynekoya <101224368+Dwaynekoya@users.noreply.github.com>
2023-11-20 10:50:18 +0100

    windows theme

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import view.MainGUIWindow;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             new MainGUIWindow().setVisible(true);
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
         }
     }
